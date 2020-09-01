7835646f01b3168b7c2b92b9ac35c647f1977f88
hazem osama ibrahim <hazem3500@gmail.com>
2020-09-01 08:05:18 +0200

    update webpack config

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,25 @@
 const path = require('path');
 
 module.exports = {
-  entry: './src/index.js',
-  output: {
-    filename: 'main.js',
-    path: path.resolve(__dirname, 'dist'),
-  },
+    entry: './src/index.js',
+    output: {
+        filename: 'main.js',
+        library: 'stylis-plugin-logical',
+        libraryTarget: 'umd',
+        path: path.resolve(__dirname, 'dist'),
+        globalObject: "typeof self !== 'undefined' ? self : this",
+    },
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                use: {
+                    loader: 'babel-loader',
+                    options: {
+                        presets: ['@babel/preset-env'],
+                    },
+                },
+            },
+        ],
+    },
 };
